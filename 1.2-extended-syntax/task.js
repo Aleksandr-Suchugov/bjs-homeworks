0895cc3b13afe46b9d2fad2aac79ad17a9d4d153
Aleksandr Suchugov <aleksandr.kross@gmail.com>
2020-05-11 14:09:15 +0300

    Second lesson is completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,47 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    `use strict`
+    let equationResult = [];
+    if (a === 0) {
+        equationResult[0] = `Это уровнение линейное. У него один корень.`;
+        equationResult[1] = -c / b;
+    }
+    let discreminant = Math.pow(b, 2) - 4 * a * c;
+    if (discreminant > 0) {
+        equationResult[0] = (( -b - Math.sqrt(discreminant)) / (2 * a)).toFixed(3);
+        equationResult[1] = (( -b + Math.sqrt(discreminant)) / (2 * a)).toFixed(3);
+    }
+    else if (discreminant === 0) {
+        equationResult[0] = 'Дискрименант равен нулю - уравнение имеет один корень.'
+        equationResult[1] = -b / (2 * a);
+    }
+    else {
+        equationResult[0] = 'Дискрименант меньше нуля - уравнение не имеет кореней.'
+        
+    }
+    return equationResult;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = [];
+    let sum = 0;
+    let countMarks = 5;
+    if (marks.length > 5) {
+        averageMark[0] = `Вы ввели больше пяти оценок. Средний бал будет вычислен только для первых пяти из введеных.`;
+    }
+    else {
+        averageMark[0] = `Средний бал вычислен введеных оценок.`;
+        countMarks = marks.length;
+    }
+    for (i = 0; i < countMarks; i++) {
+        sum = sum + marks[i];
+    }
+    averageMark[1] = sum / countMarks;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let actualYear = new Date();
+    let adultFlag = actualYear.getFullYear() - dateOfBirthday.getFullYear();
+    result = (adultFlag >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    return result;
 }
