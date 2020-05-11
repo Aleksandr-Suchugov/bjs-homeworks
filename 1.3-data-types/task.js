5fbc5ddcbf45979578644e58b16bfd399351d9c0
Aleksandr Suchugov <aleksandr.kross@gmail.com>
2020-05-12 01:44:23 +0300

    3rd task is completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict"
+    let loanRate = parseInt(percent) / 100;
+    let firstPayment = parseInt(contribution);
+    let objectPrice = parseInt(amount);
+    let actualDate = new Date();
+    let loanParams = {
+        rate: loanRate * 100,
+        loanAmount: 0,
+        monthsCount: 0,
+        fullCostOfFunds: 0
+    }
+    let totalAmount = 0;
+    if ((date.getFullYear() >= actualDate.getFullYear()) && (date.getMonth() < 12) && (date.getMonth() >= 0) && (date.getDate() < 32) && (date.getDate() >= 1)) {
+        loanParams.loanAmount = objectPrice - firstPayment;
+        loanParams.monthsCount = (date.getFullYear() - actualDate.getFullYear()) * 12 + date.getMonth() - actualDate.getMonth();
+        let annuityPayment = loanParams.loanAmount * loanRate / 12 * (1 + 1 / (Math.pow(1 + loanRate / 12, loanParams.monthsCount) - 1));
+        loanParams.fullCostOfFunds = (annuityPayment * loanParams.monthsCount).toFixed(2);
+        
+        totalAmount = loanParams.fullCostOfFunds;
+    }
+    else {
+        totalAmount = 'Неверный формат даты. Дату необходимо указать в следующем формате - ДД.ММ.ГГГГ.'
+    }
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = 0;
+    if (name) {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    }
+    else {
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+    }
+    return greeting;
 }
