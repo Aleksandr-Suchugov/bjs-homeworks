b715fd4263d2685f587f2707e0cb41119520d6e2
Aleksandr Suchugov <aleksandr.kross@gmail.com>
2020-05-12 18:52:19 +0300

    Fixes were added to Lesson2.tasks

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,47 +1,43 @@
+`use strict`
+
 function getResult(a,b,c){
-    `use strict`
     let equationResult = [];
     if (a === 0) {
-        equationResult[0] = `Это уровнение линейное. У него один корень.`;
-        equationResult[1] = -c / b;
+        equationResult[0] = -c / b;
+        return equationResult;
     }
-    let discreminant = Math.pow(b, 2) - 4 * a * c;
-    if (discreminant > 0) {
-        equationResult[0] = (( -b - Math.sqrt(discreminant)) / (2 * a)).toFixed(3);
-        equationResult[1] = (( -b + Math.sqrt(discreminant)) / (2 * a)).toFixed(3);
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
+    if (discriminant > 0) {
+        equationResult[0] = parseInt((( -b + Math.sqrt(discriminant)) / (2 * a)).toFixed(0));
+        equationResult[1] = parseInt((( -b - Math.sqrt(discriminant)) / (2 * a)).toFixed(0));
+        return equationResult;
     }
-    else if (discreminant === 0) {
-        equationResult[0] = 'Дискрименант равен нулю - уравнение имеет один корень.'
-        equationResult[1] = -b / (2 * a);
-    }
-    else {
-        equationResult[0] = 'Дискрименант меньше нуля - уравнение не имеет кореней.'
-        
+    else if (discriminant === 0) {
+        equationResult[0] = -b / (2 * a);
+        return equationResult;
     }
     return equationResult;
 }
 
 function getAverageMark(marks){
-    let averageMark = [];
-    let sum = 0;
-    let countMarks = 5;
-    if (marks.length > 5) {
-        averageMark[0] = `Вы ввели больше пяти оценок. Средний бал будет вычислен только для первых пяти из введеных.`;
+    let averageMark = 0;
+    if (marks.length > 0) {
+        let sum = 0;
+        let countMarks = 5;
+        if (marks.length < 5) {
+            countMarks = marks.length;
+        }
+        for (i = 0; i < countMarks; i++) {
+            sum = sum + marks[i];
+        }
+        averageMark = sum / countMarks;
+        return averageMark;
     }
-    else {
-        averageMark[0] = `Средний бал вычислен для введеных оценок.`;
-        countMarks = marks.length;
-    }
-    for (i = 0; i < countMarks; i++) {
-        sum = sum + marks[i];
-    }
-    averageMark[1] = sum / countMarks;
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    let actualYear = new Date();
-    let adultFlag = actualYear.getFullYear() - dateOfBirthday.getFullYear();
+    let adultFlag = new Date().getFullYear() - dateOfBirthday.getFullYear();
     result = (adultFlag >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     return result;
 }
