7407496141f3aee42ef2f9abbcabc626bcc8457f
Aleksandr Suchugov <aleksandr.kross@gmail.com>
2020-05-16 00:28:44 +0300

    task 2.2 is completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,33 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    let sound;
+    if (animal) {
+        sound = animal.sound;
+    } 
+    else {
+        sound = null;
+    }
+    animalSound = sound;
+    return animalSound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let averageMark = 0;
+    if (marks.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < marks.length; i++) {
+            sum = sum + marks[i];
+        }
+        averageMark = Math.round(sum / marks.length);
+        return averageMark;
+    }
+    return averageMark;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Date.now();
+    const diff = (now - Number(birthday)) / (365.25 * 24 * 60 * 60 * 1000);
+    let verdict = false;
+    if (diff > 17)
+        verdict = true;
+    return verdict
 }
